cdcb9394c025e98b212efd70e0162b4dbc4b670e
Fletscherrrr <115947237+Fletscherrrr@users.noreply.github.com>
2023-09-05 08:26:25 +0200

    Update baseinstall.sh

--- baseinstall.sh
+++ baseinstall.sh
@@ -1,4 +1,3 @@
 xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree -y && 
 xbps-install dbus elogind base-devel xorg wget tldr neofetch inxi power-profiles-daemon upower sof-firmware cantarell-fonts freetype noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra ttf-bitstream-vera ttf-opensans xz zip unzip ntp pipewire wireplumber alsa-firmware alsa-pipewire alsa-utils pavucontrol xdg-utils xdg-user-dirs poppler-glib libdvdcss ffmpegthumbnailer bash-completion bluez libbluetooth bluez-alsa dnsmasq ModemManager NetworkManager broadcom-bt-firmware b43-fwcutter xf86-input-libinput mesa libwnck libwnck2 -y &&
-ln -s /etc/sv/dbus /var/service &&
-ln -s /etc/sv/ntpd /var/service &&
+ln -s /etc/sv/ntpd /var/service 
