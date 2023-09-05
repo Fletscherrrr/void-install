c73aa77696b591c2d164e246bc45d66101a7c7ed
Fletscherrrr <115947237+Fletscherrrr@users.noreply.github.com>
2023-09-06 01:57:12 +0200

    Update baseinstall.sh

--- baseinstall.sh
+++ baseinstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree -y && 
 xbps-install kakoune dbus elogind base-devel xorg wget tldr neofetch inxi power-profiles-daemon upower sof-firmware xz zip unzip ntp pipewire wireplumber pavucontrol-qt rtkit xdg-utils xdg-user-dirs poppler-glib libdvdcss ffmpegthumbnailer bash-completion bluez libbluetooth ModemManager NetworkManager broadcom-bt-firmware b43-fwcutter xf86-input-libinput mesa libwnck libwnck2 kde5 kde5-baseapps -y &&
 ln -s /etc/sv/ntpd /var/service &&
