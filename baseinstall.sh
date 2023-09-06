345adc0709b390b98a59d25d6968436899da26da
Fletscherrrr <115947237+Fletscherrrr@users.noreply.github.com>
2023-09-06 05:27:07 +0200

    Update baseinstall.sh

--- baseinstall.sh
+++ baseinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # base packages + some service links
 xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree -y && 
-xbps-install kakoune dbus elogind base-devel xorg wget tldr neofetch inxi power-profiles-daemon upower sof-firmware cantarell-fonts freetype noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra ttf-bitstream-vera ttf-opensans xz zip unzip ntp pipewire wireplumber pavucontrol-qt rtkit xdg-utils xdg-user-dirs poppler-glib libdvdcss ffmpegthumbnailer bash-completion bluez libbluetooth ModemManager NetworkManager broadcom-bt-firmware b43-fwcutter xf86-input-libinput mesa libwnck libwnck2 kde5 kde5-baseapps -y &&
+xbps-install kakoune dbus elogind base-devel xorg wget tldr neofetch inxi power-profiles-daemon upower firefox sof-firmware cantarell-fonts freetype noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra ttf-bitstream-vera ttf-opensans xz zip unzip ntp pipewire wireplumber pavucontrol-qt rtkit xdg-utils xdg-user-dirs poppler-glib libdvdcss ffmpegthumbnailer bash-completion bluez libbluetooth ModemManager NetworkManager broadcom-bt-firmware b43-fwcutter xf86-input-libinput mesa libwnck libwnck2 kde5 kde5-baseapps -y &&
 ln -s /etc/sv/ntpd /var/service &&
 ln -s /etc/sv/dbus /var/service &&
 ln -s /etc/sv/NetworkManager /var/service &&
