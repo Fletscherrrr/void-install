e658798bbd0547f3e28fe46da3ab79527b4d1221
Fletscherrrr <115947237+Fletscherrrr@users.noreply.github.com>
2023-09-07 02:10:39 +0200

    Update session-start-method.sh

--- session-start-method.sh
+++ session-start-method.sh
@@ -4,14 +4,13 @@ Options=("sddm" "startx")
 select choice in "${Options[@]}"; do
         case $choice in
                 "sddm")
-                  echo "you have chosen sddm"
-                  xbps-install sddm sddm-kcm &&
-                  ln -s /etc/sv/sddm /var/service
+                  echo "you have chosen sddm"                
+                  sudo ln -s /etc/sv/sddm /var/service
                   break
                   ;;
                 "startx")
                   echo "you have chosen to startx"
-                  mv ~/void-install/.xinitrc ~/
+                  sudo mv ~/void-install/.xinitrc ~/
                   break
                   ;;
                 *) echo "invalid choice dude, it can't be this hard"
